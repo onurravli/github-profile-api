74f80fe64fdb2e749dadc24d1a5900cc758110a3
onurravli <onurravli@hotmail.com>
2023-03-19 11:56:43 +0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -2,9 +2,18 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
+def api_usage():
+    print('''
+    Usage:
+        python index.py
+    ''')
+
+
 @app.route('/')
 def home():
     return 'Hello, World!'
+
 
 @app.route('/about')
 def about():
